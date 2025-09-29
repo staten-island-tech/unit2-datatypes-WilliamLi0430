fdd2d7ddd5cef69be29a94f8429ca26333a31d0e
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-09-29 09:24:11 -0400

    e

--- gcf.py
+++ gcf.py
@@ -1,11 +1,9 @@
 first = int(input("What is your first number? "))
 second = int(input("What is your second number? "))
 
-def get_Luke(first):
-    Luke = []
-    for i in range (1, first + 1):
-        if first % i == 0:
-            Luke.append (i)
-    return Luke
+def wgfhrojg(first, second):
+    while first !=0:
+        first, second = second, first % second
+    return first
 
-print(get_Luke(Zheng))
+print(wgfhrojg)
--- numberguess.py
+++ numberguess.py
@@ -1,8 +1,8 @@
 import random
-number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
 
 while True:
-    x = int(input("Guess the number between 1 and 10 "))
+    x = int(input("Guess the number between 1 and 30 "))
 
     if x > number:
         print("Less")
