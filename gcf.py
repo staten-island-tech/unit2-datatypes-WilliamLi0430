b066f07a6b20170622142fb384b1bfb8ed7746ca
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-09-30 09:24:15 -0400

    e

--- gcf.py
+++ gcf.py
@@ -1,9 +1,10 @@
 first = int(input("What is your first number? "))
 second = int(input("What is your second number? "))
 
-def wgfhrojg(first, second):
-    while first !=0:
-        first, second = second, first % second
-    return first
-
-print(wgfhrojg)
+def wuergbo(first, second):
+    gcf = 0
+    for i in range(1, first):
+        if first % i == 0 and second % i == 0:
+            gcf = i
+    return gcf
+print(wuergbo(first, second))
--- numberguess.py
+++ numberguess.py
@@ -1,13 +1,17 @@
 import random
 number = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
+guess_history = []
 
 while True:
     x = int(input("Guess the number between 1 and 30 "))
 
     if x > number:
+        guess_history.append(x)
         print("Less")
     elif x < number:
+        guess_history.append(x)
         print("Greater")
     else:
         print("Correct")
+        print(guess_history)
         break
